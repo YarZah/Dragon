9547443e4e5cbfebdd242eebe546ce5f22f264f7
YarZah <jarikzagreb16@gmail.com>
2024-10-15 23:56:33 +0200

    Add language

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args){
         Dragon dragonNew = new Dragon( "Drago", 150, 750);
         Dragon dragon2 = new Dragon();
-        System.out.println(dragonNew.toString());
-        System.out.println(dragon2.toString());
+        System.out.println("Створено повноцінного дракона " + dragonNew.toString());
+        System.out.println("Створено невідомго дракона " + dragon2.toString());
+
     }
 }
